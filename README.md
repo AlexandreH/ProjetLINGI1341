b362a041e02c6927e80db090d32fd037aebeb18b
Alexandre Halbardier <ahalbardier@jabba.info.ucl.ac.be>
2018-11-02 22:12:30 +0100

    Ajouts test + écriture README

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# ProjetLINGI1341
+# ProjetLINGI1341 : Truncated Reliable Transport Protocol par Noémie Nicaise Et Alexandre Halbardier
+
+Pour lancer sender et receiver : 
+
+A l'aide votre terminal, entrez dans le dossier ProjetLINGI1341 et tappez make pour compiler le projet.
+
+Ensuite vous pouvez lancer le sender en tappant par exemple en local :/sender localhost 12345
+
+Vous pouvez ensuite lancer votre receiver dans une autre fenetre de terminal en tappant :/receiver localhost 12345
+
+Dans le terminal de votre sender, tappez un message, il apparaitra dans le terminal de votre receiver!
+
+
+Pour lancer les tests :
+
+Entrez également dans le dossier ProjetLINGI1341 avec votre terminal.
+
+Tappez make test, et la série de tests sera effectuée.
--- tests/test.c
+++ tests/test.c
@@ -23,7 +23,7 @@
 /* Séries de tests pour le Projet LINGI1341*/
 
 
-/*Premiere serie de tests sur packet_implem.c*/
+/*Tests sur packet_implem.c*/
 
 void test_pkt_new(void)
 {
@@ -72,10 +72,56 @@ void test_set_window(void)
   CU_ASSERT_NOT_EQUAL(status,PKT_OK);//taille invalide
 }
 
-void test_5(void)
+void test_set_payload(void)
 {
-  CU_ASSERT_STRING_EQUAL("string aaa", "string aaa");
+	pkt_t* pkt = pkt_new();
+	pkt_status_code status=PKT_OK;
+	
+	char *testpetit = "123456789"; //plus petit que MAX_PAYLOAD_SIZE
+	
+	char testgrand[2000];
+	int i;
+	for(i=0;i<2000;i++){testgrand[i]='a';} //plus grand que MAX_PAYLOAD_SIZE
+	
+	status = pkt_set_payload(pkt,testpetit,10);
+	CU_ASSERT_EQUAL(status,PKT_OK);//payload à la bonne taille
+	 
+	 status = pkt_set_payload(pkt,testgrand,2000);
+	 CU_ASSERT_NOT_EQUAL(status,PKT_OK);//payload pas à la bonne taille
+	
 }
+
+/*
+ * Tests sur send_receive
+ */
+
+void test_real_address(void) //on teste la creation d'une sockaddr_in6 à partir d'un nom d'hote
+{
+	struct sockaddr_in6 ipv6;
+	const char *testreal1 = real_address("localhost",&ipv6);//localhost est un nom d'adresse connu par l'ordinateur
+	CU_ASSERT_EQUAL(NULL,testreal1);
+	
+	const char *testreal2 = real_address("nomrandom",&ipv6);//nom d'adresse pas reconnu par l'ordinateur
+	CU_ASSERT_NOT_EQUAL(NULL,testreal2);
+	
+	}
+	
+	void test_create_socket(void) //on teste la creation d'un socket
+{
+	struct sockaddr_in6 ipv6first;
+	const char *test1 = real_address("localhost",&ipv6first);
+	CU_ASSERT_EQUAL(NULL,test1);
+	
+	struct sockaddr_in6 ipv6second;
+	const char *test2 = real_address("localhost",&ipv6second);
+	CU_ASSERT_EQUAL(NULL,test2);
+	
+	int fdtest= create_socket(&ipv6first,10000,&ipv6second,12345);//on peut choisir les ports aléatoirement
+	CU_ASSERT_NOT_EQUAL(fdtest,-1); //fdtest retournerait -1 s'il y avait une erreur
+	
+	}
+
+
 int setup(void)  {return 0;}
 int teardown(void) {return 0;}
 
@@ -83,23 +129,49 @@ int main()
 {
 	if (CUE_SUCCESS != CU_initialize_registry()){return CU_get_error();}
 	CU_pSuite pSuite = NULL;
-	pSuite = CU_add_suite("Premiere suite de tests", setup, teardown);
+	
+	pSuite = CU_add_suite("Tests de packet_implem", setup, teardown);
+	
 	if(NULL == pSuite) 
 	{CU_cleanup_registry();
 		return CU_get_error();
 		}
 	
-	if ((NULL == CU_add_test(pSuite, "isTrue",test_pkt_new)) ||
-    (NULL == CU_add_test(pSuite, "isNotEqual", test_set_type)) ||
-    (NULL == CU_add_test(pSuite, "isEqualString", test_set_tr)) ||
-    (NULL == CU_add_test(pSuite, "IsFalse", test_4)) ||
-    (NULL == CU_add_test(pSuite, "isEqualString", test_5)))
+	if ((NULL == CU_add_test(pSuite, "pkt_new",test_pkt_new)) ||
+    (NULL == CU_add_test(pSuite, "set_type", test_set_type)) ||
+    (NULL == CU_add_test(pSuite, "set_tr", test_set_tr)) ||
+    (NULL == CU_add_test(pSuite, "set_window", test_set_window)) ||
+    (NULL == CU_add_test(pSuite, "set_payload", test_set_payload)))
     {
 		printf("Tous les tests ne se sont pas effectue avec succes\n");
 		CU_cleanup_registry();
 		return CU_get_error();
     }
-	printf("\nVoici les resultat de notre premiere suite de tests:\n");
+    
+    
+    pSuite = CU_add_suite("Tests de send_receive", setup, teardown);
+	
+	if(NULL == pSuite) 
+	{CU_cleanup_registry();
+		return CU_get_error();
+		}
+		
+		if ((NULL == CU_add_test(pSuite, "real_adress",test_real_address))||
+		(NULL == CU_add_test(pSuite, "create socket",test_create_socket))
+		)
+    {
+		printf("Tous les tests ne se sont pas effectue avec succes\n");
+		CU_cleanup_registry();
+		return CU_get_error();
+    }
+    
+    
+	printf("\nVoici les resultat de nos de tests:\n");
+	printf("\nChaque suite correspond à un fichier de notre dossier src\n");
+	printf("Chaque test correspond a une fonction.\n");
+	printf("Il peut y avoir un ou plusieurs asserts par test.\n");
+	printf("Seules les suites contenant des erreurs apparaitront dans les détails.\n");
+	
 	CU_basic_run_tests();
 	CU_basic_show_failures(CU_get_failure_list());
 	CU_cleanup_registry();
